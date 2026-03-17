9c8bc7022fe40d656fac4657553f4903e6f5fdfc
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-03-17 12:35:23 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-02b2c0c8175091ab8" # replace with your SG ID
+SG_ID="sg-0d33396b3a43651ba" # replace with your SG ID sg-0d33396b3a43651ba 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z04626924Z2WBLM8PDBQ" # replace with your ZONE ID
-DOMAIN_NAME="kimidi.site" # replace with your domain
+ZONE_ID="Z05669211QZY6H9HOXJ4R" # replace with your ZONE ID
+DOMAIN_NAME="somaraju.online" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
-do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-02b2c0c8175091ab8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0d33396b3a43651ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
