3a678acaa150ef1a025a80b2788275d39cfe4b6a
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-03-18 09:01:02 +0530

    catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -56,6 +56,7 @@ VALIDATE $? "creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "downloading catalogue"
 
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzipping catalogue"
