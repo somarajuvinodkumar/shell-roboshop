d5d60cee96fb9822077bdf3b7ba16ec8b86649c3
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-03-18 08:31:13 +0530

    catalogue.sh

---
+++ catalogue.sh
@@ -0,0 +1,85 @@
+
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y 
+VALIDATE $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enabling nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+if [ $? -ne 0 ]
+then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "creating roboshop system user"
+else
+    echo -e "system user roboshop already created ...$Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading catalogue"
+
+cd /app
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping catalogue"
+
+npm install
+VALIDATE $? "npm dependencies installing"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "copying catalogue service"
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+VALIDATE $? "starting catalogue"
+
+cp $SCRIPT_DIR/mongod.repo /etc/yum.repos.d/mongod.repo
+dnf install mongodb-mongosh -y
+VALIDATE $? "installing mongodb client"
+
+STATUS=$(mongosh --host mongodb.kimidi.site --eval 'db.getMongo().getDBnames().indexof("catalogue")')
+if [ STATUS -lt 0 ]
+then 
+     mongosh --host mongodb.somaraju.online </app/db/master-data.js &>>$LOG_FILE 
+     VALIDATE $? "Loading data into mongodb"
+else
+    echo -e "data is already loaded ....$Y SKIPPING $N" 
+fi
