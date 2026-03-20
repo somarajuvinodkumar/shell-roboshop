a49a695a743d6a085adaecd72071cd737d6705a9
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-03-20 08:42:08 +0530

    cart.sh

---
+++ cart.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "disabling nodejs"
+
+ dnf module enable nodejs:20 -y
+ VALIDATE $? "enabling nodejs"
+
+ dnf install nodejs -y
+ VALIDATE $? "installing nodejs"
+
+ if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "creating roboshop system user"
+else
+    echo -e "system user roboshop alreday created $Y...skipping $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+VALIDATE $? "downloading cart"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/cart.zip
+VALIDATE $? "unzipping cart"
+
+npm install
+VALIDATE $? "installing npm dependencies"
+
+cp "$SCRIPT_DIR/cart.service" /etc/systemd/system/cart.service &>>"$LOG_FILE"
+VALIDATE $? "copying cart service"
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
+VALIDATE $? "starting cart"
